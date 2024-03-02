0399ae86d7cb32ddfca87bddd0f25f10b6489940
Abdulmalik Ariyo <malikariyo@hotmail.com>
2024-03-02 09:21:21 -0700

    Initial Commit

---
+++ CCAircraft/CCAircraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCAircraftApp.swift
+//  CCAircraft
+//
+//  Created by Abdulmalik Ariyo on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CCAircraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCAircraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CCAircraft
+//
+//  Created by Abdulmalik Ariyo on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
